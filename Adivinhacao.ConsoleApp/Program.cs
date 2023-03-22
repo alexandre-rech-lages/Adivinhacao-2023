1942d477e397ecc2dccbb51d8e6a5f7cfddb1174
Alexandre Rech <alexandre.rech.lages@gmail.com>
2023-03-21 21:47:56 -0300

    Reorganizado os requisitos

--- Adivinhacao.ConsoleApp/Program.cs
+++ Adivinhacao.ConsoleApp/Program.cs
@@ -1,30 +1,32 @@
-﻿namespace Adivinhacao.ConsoleApp
+﻿#region Requisito 1 - OK
+/*
+    O computador pensará em um número, e você, jogador, precisará adivinhá-lo.
+    A cada erro, a máquina lhe dirá se o número chutado foi
+    maior ou menor do que o pensado.
+    No acerto, deverá parabenizar o jogador.
+*/
+#endregion
+
+#region Requisito 2 - OK
+/*
+    Você também poderá escolher o nível de dificuldade do jogo, 
+    e isso lhe dará mais ou menos oportunidades de chutar um número.
+*/
+#endregion
+
+#region Requisito 3 - OK
+/*
+    Ao final, se você ganhar, o computador lhe dirá quantos pontos você fez,
+    baseando-se em quão bons eram seus chutes.
+*/
+#endregion
+
+
+namespace Adivinhacao.ConsoleApp
 {
     internal class Program
     {
-        #region Requisito 1 - OK
-        /*
-            O computador pensará em um número, e você, jogador, precisará adivinhá-lo.
-            A cada erro, a máquina lhe dirá se o número chutado foi
-            maior ou menor do que o pensado.
-            No acerto, deverá parabenizar o jogador.
-        */
-        #endregion
-
-        #region Requisito 2 - OK
-        /*
-            Você também poderá escolher o nível de dificuldade do jogo, 
-            e isso lhe dará mais ou menos oportunidades de chutar um número.
-        */
-        #endregion
-
-        #region Requisito 3 - OK
-        /*
-            Ao final, se você ganhar, o computador lhe dirá quantos pontos você fez,
-            baseando-se em quão bons eram seus chutes.
-        */
-        #endregion
-
+       
         static void Main(string[] args)
         {
             int nivelDificuldade = 1, totalDeTentativas = 0;
